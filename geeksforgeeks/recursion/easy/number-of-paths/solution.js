ccc65a1fcf3b29b4bf90209dc15b5e890d4332eb
kroy <kroy@expediagroup.com>
2022-04-03 18:35:37 -0700

    add practice

--- geeksforgeeks/recursion/easy/number-of-paths/solution.js
+++ geeksforgeeks/recursion/easy/number-of-paths/solution.js
@@ -1,4 +1,13 @@
 // https://practice.geeksforgeeks.org/problems/number-of-paths0926/1
 
-export const solution = () => {
+export const solution = (m, n) => {
+  const numOfPaths = (x, y) => {
+    if (x === m && y === n) {
+      return 1;
+    } else if (x > m || y > n) {
+      return 0;
+    }
+    return numOfPaths(x + 1, y) + numOfPaths(x, y + 1)
+  }
+    return numOfPaths(1, 1);
 }
--- geeksforgeeks/recursion/easy/number-of-paths/solution.spec.js
+++ geeksforgeeks/recursion/easy/number-of-paths/solution.spec.js
@@ -4,11 +4,11 @@ describe('test name', () => {
   it('test 1', () => {
     // Arrange
     const m = 3;
-    const n = 2;
+    const n = 3;
     const expected = 6;
 
     // Act
-    const result = solution();
+    const result = solution(m, n);
 
     // Assert
     expect(result).toEqual(expected);
@@ -20,7 +20,7 @@ describe('test name', () => {
     const expected = 8;
 
     // Act
-    const result = solution();
+    const result = solution(m, n);
 
     // Assert
     expect(result).toEqual(expected);
